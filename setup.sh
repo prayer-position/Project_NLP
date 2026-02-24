e28f0746135cdf85c574a3b79abad659a71998ee
prayer-position <charlesdidiermoi@gmail.com>
2026-02-24 19:36:54 +0100

    Accepted changes

--- setup.sh
+++ setup.sh
@@ -3,8 +3,4 @@ python3 -m venv venv_nlp
 source venv_nlp/Scripts/activate
 pip3 install --upgrade pip
 pip install -r requirements.txt
-<<<<<<< HEAD
-
-=======
->>>>>>> 29d1af0f55e92611661ca5585fafc6805986fbba
 chmod 755 setup.sh #rwx->u and rx->g/o
